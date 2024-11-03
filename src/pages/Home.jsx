c805211b6c2fc16e380ea2a5c41a43c1748b9ab9
marianialmeida28 <marianialmeida00@gmail.com>
2024-11-03 00:14:03 -0300

    03/11

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,29 @@
-import { useEffect, usestate } from "react";
-import ListarProdutos from "•/ListarProdutos";
+import { useEffect, useState } from "react";
+import ListarProdutos from "./ListarProdutos";
 
- export default function Home() {
-    const 
- }
+export default function Home() {
+    const [produtos, setProdutos] = useState([]);
+
+    useEffect(() => {
+        const receberListaProdutos = async () => {
+            try {
+                const resposta = await fetch('https://fakestoreapi.com/products');
+                const dados = await resposta.json();
+                setProdutos(dados);
+            } catch (erro) {
+                alert('Ocorreu um erro na comunicação com o servidor');
+            }
+        };
+        receberListaProdutos();
+    }, []);
+
+    return (
+        <>
+            <header>
+                <h1>Produtos</h1>
+            </header>
+            <ListarProdutos lista={produtos} />
+        </>
+    );
+}
     
---
+++ src/pages/ListarProdutos.jsx
@@ -0,0 +1,18 @@
+import styles from '../styles/listarProdutos.module.css';
+
+export default function ListarProdutos({ lista }) {
+    return (
+        <>
+            <ul className={styles.bloco}>
+                {lista.map(produto => (
+                    <li key={produto.id}>
+                        <img src={produto.image} alt={produto.title} className={styles.imagem} />
+                        <h2>{produto.title}</h2>
+                        <p>{produto.description}</p>
+                        <button className={styles.precoBotao}>R${produto.price}</button>
+                    </li>
+                ))}
+            </ul>
+        </>
+    );
+}
